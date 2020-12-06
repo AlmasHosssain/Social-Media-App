968f021f2e5f018cc0ce93765d3c87336f3f963c
Almas Hossain <almashossain121@gmail.com>
2020-12-06 11:40:54 -0800

    Update test.js

--- controllers/test.js
+++ controllers/test.js
@@ -35,40 +35,6 @@ testEmail=(req,res)=>{
    })
 }
 
-testAgain =(req,res)=>{
-   let {email} = req.body
-   nodemailer.createTestAccount((err,account)=>{
-      const htmlEmail = `
-         <b>Click Here to change your password : </b>
-         <a>http://localhost:3000/changePassword</a>
-      `
-      let transform = nodemailer.createTransport({
-         host : "smtp.gmail.com",
-         port : 587,
-         secure : false,
-         auth : {
-            user : 'almashossain121@gmail.com',
-            pass : '*********'
-         }   
-      })
-
-      let sendMail= {
-         form : 'vivacode@gmail.com',
-         to: email,
-         subject : 'Viva check',
-         text : "Please pray for me. So that I can qualified",
-         html : htmlEmail
-      }
-
-      transform.sendMail(sendMail,(err,info)=>{
-         if (err) {
-            return console.log(err)
-         }else{
-            console.log(`Message send successfully`)
-         }
-      })
-   })
-}
 
 module.exports = {
    testEmail
